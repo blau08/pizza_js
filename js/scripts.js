0d9340a75b1ac181cf1d178a14a5d4363de7b366
Brian Lau <blau08@gmail.com>
2015-09-18 11:17:16 -0700

    Clear inputs with Jquery after submitting form

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ function Pizza(size, toppings, quantity) {
 Pizza.prototype.pizzaPrice = function() {
   var price = 0;
 
-  if (this.pizzaSize = 1) {
+  if (this.pizzaSize == "Small") {
     price += 5;
-  } else if (this.pizzaSize = 2) {
+  } else if (this.pizzaSize == "Medium") {
     price += 10;
-  } else if (this.pizzaSize = 3) {
+  } else if (this.pizzaSize == "Large") {
     price += 15;
   }
 
@@ -45,15 +45,17 @@ Pizza.prototype.pizzaPrice = function() {
 $(document).ready(function() {
 
   var pizzaSize = ["Small", "Medium", "Large"];
+  // var pizzaSize = {1: "Small", 2: "Medium", 3: "Large"};
+  var pizzaSize2 = [1, 2, 3];
   var pizzaToppings = [1, 2, 3, 4, 5];
   var pizzaQuantity = [1, 2, 3, 4, 5];
 
   pizzaSize.forEach(function(size) {
-    $("select#size-list").append("<option><span class='chosen-size'>" + size + "</span></option>");
+    $("select#size-list").append("<option value='" + size + "'><span class='chosen-size'>" + size + "</span></option>");
   });
 
   pizzaToppings.forEach(function(toppings) {
-    $("select#toppings-list").append("<option value='" + toppings + "'><span class='chosen-toppings'>" + toppings + ":00 PM" + "</span></option>");
+    $("select#toppings-list").append("<option value='" + toppings + "'><span class='chosen-toppings'>" + toppings + "</span></option>");
   });
 
   pizzaQuantity.forEach(function(quantity) {
@@ -65,14 +67,16 @@ $(document).ready(function() {
     var pizzaChoice = $("#size-list option:selected").text();
     var toppingsChoice = parseInt($("#toppings-list option:selected").val());
     var quantityChoice = parseInt($("#quantity-list option:selected").val());
-    debugger;
     userPizza = new Pizza(pizzaChoice, toppingsChoice, quantityChoice);
 
-    userPrice = userPizza.pizzaPrice();
+    var userPrice = userPizza.pizzaPrice();
 
     $(".pizza-price").text(userPrice);
 
     $("#result").show();
     event.preventDefault();
+    $("select#size-list").val("");
+    $("select#toppings-list").val("");
+    $("select#quantity-list").val("");
   });
 });

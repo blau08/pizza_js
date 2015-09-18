b097717e920b10511324763c07e2d30f5f212a43
Brian Lau <blau08@gmail.com>
2015-09-18 11:23:49 -0700

    Don't know how to spec test html form to be empty

--- specs/specs.js
+++ specs/specs.js
@@ -1,13 +1,19 @@
 describe("Pizza", function() {
   it("creates a new pizza with the given specifications", function() {
-    var testPizza = new Pizza("small", "1", "1");
-    expect(testPizza.pizzaSize).to.equal("small");
+    var testPizza = new Pizza("Small", "1", "1");
+    expect(testPizza.pizzaSize).to.equal("Small");
     expect(testPizza.pizzaToppings).to.equal("1");
     expect(testPizza.pizzaQuantity).to.equal("1");
   });
 
   it("will return the price of the pizza", function() {
-    var testPizza = new Pizza("small", "1", "1");
+    var testPizza = new Pizza("Small", "1", "1");
     expect(testPizza.pizzaPrice()).to.equal("6");
   });
+
+  // it("clears form after submit button", function() {
+  //   var testPizza = new Pizza("Small", "1", "1");
+  //   expect(testPizza.pizzaPrice()).to.equal("6");
+  //   expect($("select#size-list").to.equal(""));
+  // });
 });

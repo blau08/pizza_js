d7519f7422aa78ae8f43d97901512e84d84d5058
Brian Lau <blau08@gmail.com>
2015-09-18 09:57:17 -0700

    Pass first pizza price spec

--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,13 @@
+describe("Pizza", function() {
+  it("creates a new pizza with the given specifications", function() {
+    var testPizza = new Pizza("small", "1", "1");
+    expect(testPizza.pizzaName).to.equal("small");
+    expect(testPizza.pizzaTime).to.equal("1");
+    expect(testPizza.userAge).to.equal("1");
+  });
 
+  it("will return the price of the pizza", function() {
+    var testPizza = new Pizza("small", "1", "1");
+    expect(testPizza.pizzaPrice()).to.equal("6");
+  });
+});
